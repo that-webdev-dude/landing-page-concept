f3d92d880aecf390d5286425c4d824b593bc3f63
iClusterDev <cluster.coredev@gmail.com>
2020-01-01 10:19:47 +0000

    cleanup started

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,48 @@
-import HelloWorldClass from "./components/HelloWorld";
+// import HelloWorldClass from "./components/HelloWorld";
 
 export default function() {
-  const hello = new HelloWorldClass();
+  // const hello = new HelloWorldClass();
+
+  class ScrollBar {
+    static hide(selector) {
+      el = document.querySelector(selector);
+      el.style.overflow = "hidden";
+    }
+
+    static show(selector) {
+      el = document.querySelector(selector);
+      el.style.overflow = "auto";
+    }
+  }
+
+  const navDrawer = {
+    active: false,
+    element: document.querySelector("#drawer"),
+    triggers: [document.querySelector("#drawer-close"), document.querySelector("#nav-menu"), ...Array.from(document.querySelectorAll(".drawer-link"))],
+    container: document.querySelector("#body")
+  };
+
+  const toggleClass = (element, className) => {
+    element.classList.contains(className) ? element.classList.remove(className) : element.classList.add(className);
+  };
+
+  const hideScrollbar = element => {
+    element.style.overflow = "hidden";
+  };
+
+  const showScrollbar = element => {
+    element.style.overflow = "auto";
+  };
+
+  const toggleState = () => {
+    navDrawer.active = !navDrawer.active;
+    navDrawer.active ? hideScrollbar(navDrawer.container) : showScrollbar(navDrawer.container);
+    toggleClass(navDrawer.element, "active");
+  };
+
+  navDrawer.triggers.forEach(trigger => {
+    trigger.addEventListener("click", () => {
+      toggleState();
+    });
+  });
 }
